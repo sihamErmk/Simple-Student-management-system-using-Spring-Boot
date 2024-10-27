7e10339e254a072a8f801d168192918e5abe3d4f
EL KOUAZE Siham <selkouaze@gmail.com>
2024-10-27 13:47:25 +0100

    first commit

---
+++ src/main/java/com/example/studentsmanagementsystem/StudentsManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentsmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentsManagementSystemApplication.class, args);
+    }
+
+}
