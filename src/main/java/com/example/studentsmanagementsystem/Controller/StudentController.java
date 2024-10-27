c785f3d2f8af8571ad766f11544b3470ae2b4390
EL KOUAZE Siham <selkouaze@gmail.com>
2024-10-27 15:25:57 +0100

    Student Controller

---
+++ src/main/java/com/example/studentsmanagementsystem/Controller/StudentController.java
@@ -0,0 +1,24 @@
+package com.example.studentsmanagementsystem.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+
+    @GetMapping
+    public List<String> findAllStudents(){
+
+        return List.of(
+                "Siham",
+                "Ereh",
+                "Mikassa"
+        );
+    }
+
+
+}
